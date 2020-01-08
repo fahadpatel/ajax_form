80ff4fa56e8330bdf42bf5019610b7a8f9e1a0cf
fahadpatel <fahad.patel93@gmail.com>
2020-01-08 17:54:08 +0500

    Update form.php

--- form.php
+++ form.php
@@ -1,5 +1,11 @@
 <!DOCTYPE html>
 <html>
+	<head>
+	<script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+	</head>	
 <body>
 
 <h2>The XMLHttpRequest Object</h2>
